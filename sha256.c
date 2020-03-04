83112e311f977fb037a25d6b4556a3329f1bd8ab
jaymz95 <jamesmullarke15@gmail.com>
2020-03-04 18:38:20 +0000

    Ch method fixed and formatted

--- sha256.c
+++ sha256.c
@@ -3,7 +3,7 @@
 
 //section 4.1.2
 uint32_t Ch(uint32_t x, uint32_t y, uint32_t z){
-    return (x & y) ^ (!x & z);
+    return (x & y) ^ (~x & z);
 }
 
 int main(int argc, char *argv[]) {
@@ -11,11 +11,11 @@ int main(int argc, char *argv[]) {
     uint32_t y = 0xcccccccc;
     uint32_t z = 0x55555555;
 
-    printf("x = %x\n", x);
-    printf("y = %x\n", y);
-    printf("z = %x\n", z);
+    printf("x         = %08x\n", x);
+    printf("y         = %08x\n", y);
+    printf("z         = %08x\n", z);
 
-    printf("Ch(x,y,z) = %x\n", Ch(x, y, z));
+    printf("Ch(x,y,z) = %08x\n", Ch(x, y, z));
 
     return 0;
 }
