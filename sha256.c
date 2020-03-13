16bdae73942a8d14e1781ade029229f9f68073c8
jaymz95 <jamesmullarkey15@gmail.com>
2020-03-13 14:38:56 +0000

    Maj function

--- sha256.c
+++ sha256.c
@@ -6,6 +6,11 @@ uint32_t Ch(uint32_t x, uint32_t y, uint32_t z){
     return (x & y) ^ (~x & z);
 }
 
+//section 4.1.2
+uint32_t Maj(uint32_t x, uint32_t y, uint32_t z){
+    return (x & y) ^ (x & z) ^ (y & z);
+}
+
 int main(int argc, char *argv[]) {
     uint32_t x = 0x0f0f0f0f;
     uint32_t y = 0xcccccccc;
