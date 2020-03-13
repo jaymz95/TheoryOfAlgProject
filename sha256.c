7db1ac06184b954075b10308b52ffe3e0bbb79bb
jaymz95 <jamesmullarkey15@gmail.com>
2020-03-13 14:53:39 +0000

    ROTR function and testing functions

--- sha256.c
+++ sha256.c
@@ -12,8 +12,13 @@ uint32_t Maj(uint32_t x, uint32_t y, uint32_t z){
 }
 
 //section 3.2
-uint32_t SHR(uint32_t x, INT N){
+uint32_t SHR(uint32_t x, int n){
     return x >> n;
+}
+
+//section 3.2
+uint32_t ROTR(uint32_t x, int n){
+    return (x >> n) | (x << (32 - n));
 }
 
 int main(int argc, char *argv[]) {
@@ -25,7 +30,11 @@ int main(int argc, char *argv[]) {
     printf("y         = %08x\n", y);
     printf("z         = %08x\n", z);
 
-    printf("Ch(x,y,z) = %08x\n", Ch(x, y, z));
+    printf("Ch(x,y,z)  = %08x\n", Ch(x, y, z));
+    printf("Maj(x,y,z) = %08x\n", Maj(x, y, z));
+
+    printf("SHR(x, 4)   = %08x\n", SHR(x, 4));//PADDED WITH 0's
+    printf("ROTR(x, 4)  = %08x\n", ROTR(x, 4));//PADDED WITH VALUE THAT WAS PUSHED OFF THE EDGE
 
     return 0;
 }
