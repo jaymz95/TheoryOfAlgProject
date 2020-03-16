0bfc1a69903c3c6f426d93baf3efe0c650941add
jaymz95 <jamesmullarke15@gmail.com>
2020-03-16 12:51:00 +0000

    Unions started

---
+++ unions.c
@@ -0,0 +1,16 @@
+#include <stdio.h>  
+#include <inttypes.h>
+
+union endian {
+	uint8_t ei[2];
+	uint16_t si;
+};
+
+int main (int argc, char *argv[]) {
+
+	union endian myunion;
+
+	myunion.si = 0xff;
+
+	return 0;
+}
