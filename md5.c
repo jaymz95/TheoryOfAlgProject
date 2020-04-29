64fcdaa0baa9d951ceaecf1708aa3ae97dcad6bb
jaymz95 <jamesmullarkey15@gmail.com>
2020-04-29 14:10:52 +0100

    getopt code fixed

--- md5.c
+++ md5.c
@@ -139,47 +139,47 @@ int nextblock(uint8_t *original_input) {
 // pointer array which points to each argument passed to the program
 int main(int argc, char *argv[]) {
 
-int aflag = 0;
-  int bflag = 0;
-  char *cvalue = NULL;
-  int index;
-  int c;
+    int aflag = 0;
+    int bflag = 0;
+    char *cvalue = NULL;
+    int index;
+    int c;
 
-  opterr = 0;
+    opterr = 0;
 
-  while ((c = getopt (argc, argv, "abc:")) != -1)
-    switch (c)
-      {
-      case 'a':
-        aflag = 1;
-        break;
-      case 'b':
-        bflag = 1;
-        break;
-      case 'c':
-        cvalue = optarg;
-        break;
-      case '?':
-        if (optopt == 'c')
-          fprintf (stderr, "Option -%c requires an argument.\n", optopt);
-        else if (isprint (optopt))
-          fprintf (stderr, "Unknown option `-%c'.\n", optopt);
-        else
-          fprintf (stderr,
-                   "Unknown option character `\\x%x'.\n",
-                   optopt);
-        return 1;
-      default:
-        abort ();
-      }
+    while ((c = getopt (argc, argv, "abc:")) != -1)
+        switch (c)
+        {
+            case 'a':
+                aflag = 1;
+                break;
+            case 'b':
+                bflag = 1;
+                break;
+            case 'c':
+                cvalue = optarg;
+                break;
+            case '?':
+                if (optopt == 'c')
+                fprintf (stderr, "Option -%c requires an argument.\n", optopt);
+                else if (isprint (optopt))
+                fprintf (stderr, "Unknown option `-%c'.\n", optopt);
+                else
+                fprintf (stderr,
+                        "Unknown option character `\\x%x'.\n",
+                        optopt);
+                return 1;
+            default:
+                abort ();
+        }
+        
+
+        printf("aflag = %d, bflag = %d, cvalue = %s\n", aflag, bflag, cvalue);
+
+        for(index = optind; index < argc; index++)
+            printf("Non-option argument %s\n", argv[index]);
+        return 0;
     }
-
-  printf ("aflag = %d, bflag = %d, cvalue = %s\n",
-          aflag, bflag, cvalue);
-
-  for (index = optind; index < argc; index++)
-    printf ("Non-option argument %s\n", argv[index]);
-  return 0;
 
     char *input;
     // Expect and open a single filename
