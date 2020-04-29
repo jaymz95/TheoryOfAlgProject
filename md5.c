39b29917d3511b5a81df9f757d16df26251389a7
jaymz95 <jamesmullarke15@gmail.com>
2020-04-29 14:27:02 +0000

    t option not working for getopt

--- md5.c
+++ md5.c
@@ -146,32 +146,21 @@ int main(int argc, char *argv[]) {
     char *cvalue = NULL;
     int index;
     int c;
-
+    //int h = "-help";	
     opterr = 0;
     //printf("%d",getopt(argc, argv, "abc:"));
-    while ((c = getopt (argc, argv, "abc:")) != -1){
-        switch (c)
+    while ((c = getopt (argc, argv, "ht:")) != -1){
+        printf("option:  %c\n",optopt);
+	switch (c)
         {
-            case '-help':
+            case 'h':
                 aflag = 1;
                 break;
-            case '-test':
+            case 't':
                 bflag = 1;
                 break;
-            case 'c':
-                cvalue = optarg;
-                break;
-            case '?':
-                if (optopt == 'c')
-                fprintf (stderr, "Option -%c requires an argument.\n", optopt);
-                else if (isprint (optopt))
-                fprintf (stderr, "Unknown option `-%c'.\n", optopt);
-                else
-                fprintf (stderr,
-                        "Unknown option character `\\x%x'.\n",
-                        optopt);
-                return 1;
             default:
+		printf("Argument not recognised!\n");
                 abort ();
         }
         
